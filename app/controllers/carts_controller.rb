e450e72b1ba48bb49add3785a0ba5b3a56a0f39c
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-12-04 18:05:07 +0900

    readme修正

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,29 @@
+class CartsController < ApplicationController
+  before_action :setup_cart_item!, only: [:add_item,:update_item, :show]
+
+  def show
+    @cart_items = CartItem.where(cart_id: current_cart.id)
+    @total_price = @cart_items.calc_total_price(@cart_items)
+  end
+
+  def add_item
+    if Cartitem.find_by(product_id: params[:product_id], cart_id: current_cart.id).blank?
+      @cart_item = current_cart_items.create(product_id: params[:product_id])
+    end
+
+    @cart_item.quantity += params[:product][:quantity].ti_i
+    @cart_item.save!
+    redirect_to current_cart
+  end
+
+  def update_item
+    @cart_item.update(quantity: params[:quantity].to_i)
+    redirect_to current_cart
+  end
+
+  private
+
+  def setup_cart_item!
+    @cart_item = CartItem.find_by(cart_id: current_cart.id,product_id: params[:product_id])
+  end
+end
