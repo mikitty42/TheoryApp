57286ef05a84b78a46b1730c8621f95647b63d15
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-12-05 19:48:13 +0900

    controller実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,60 @@
 class ProductsController < ApplicationController
+  before_action :ensure_correct_user, { only: [:new,:create,:edit,:destroy]}
+
+  def index
+    @products =  Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = "商品を登録しました"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update_attributes(product_params)
+      flash[:success] = "更新しました"
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+
+  def ensure_correct_user
+    if current_user.admin?
+    else
+      flash[:notice] = "アクセス権限がありません"
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    Product.find(params[:id]).destroy
+    flash[:success] = "商品を削除しました"
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,:price,:title,:memo,:author,:picture)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
