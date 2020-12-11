59ff53bf6ee34f9c912c2fad8a55a1c2f27537ef
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-12-11 14:00:01 +0900

    ビデオ通話機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'rooms/show'
   root to: 'users#new'
   resources :users, only: [:new,:create,:show,:index]
   resources :sessions,only: [:new,:create,:destroy]
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   resources :conversations do
     resources :messages
   end
-
+  get 'rooms/show'
 
   patch '/add_item' => 'carts#add_item'
   post '/update_item' => 'carts#update_item'
