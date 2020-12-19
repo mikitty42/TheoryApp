d81a895a0ccd47fd53feebf8b6de695c4924445f
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-12-19 23:26:37 +0900

    init unicorn

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
   require 'carrierwave/storage/file'
   require 'carrierwave/storage/fog'
   config.storage :fog
-  #config.fog_provider = 'fog/aws'
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
