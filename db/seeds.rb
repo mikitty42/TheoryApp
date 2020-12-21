f75d376101189b7115dd499c022d81d78f234302
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-12-21 09:29:59 +0900

    エラー修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-#User.create!(name:  "管理者",
-             #email: "admin@example.jp",
-             #password:  "111111",
-             #password_confirmation: "111111",
-             #admin: true)
-#Cart.create!(user_id: "1")
+User.create!(name:  "管理者",
+             email: "admin@example.jp",
+             password:  "111111",
+             password_confirmation: "111111",
+             admin: true)
+Cart.create!(user_id: "1")
 100.times do |n|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
