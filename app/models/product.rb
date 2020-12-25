f7252aa269d67fd44ee01d2099ce6c28c2de0538
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-12-25 12:26:33 +0900

    product モデル修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   has_many :favorites,dependent: :destroy
   mount_uploader :picture, PictureUploader
   validates :name,presence: true
-  validates :price, numericality: { greater_than: 1000}
+  validates :price, numericality: { greater_than_or_equal_to: 1000, less_than_or_equal_to: 1000000}
   validates :picture,presence: true
   scope :get_by_name, -> (name) { where("name LIKE ?", "%#{name}%")}
 
